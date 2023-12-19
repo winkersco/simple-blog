56972dd53c16205aae5a8ae545b0bc3af99877cf
winkersco <smmehdisharifi@gmail.com>
2023-12-20 01:09:55 +0330

    feat: implement permissions config file and seeder + add hasRoles trait to user model

---
+++ config/permissions.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    [
+        'name' => 'article-view',
+        'description' => 'view articles'
+    ],
+    [
+        'name' => 'article-publish',
+        'description' => 'publish a specific article'
+    ],
+    [
+        'name' => 'article-update',
+        'description' => 'update a specific article'
+    ],
+    [
+        'name' => 'article-delete',
+        'description' => 'delete a specific article'
+    ]
+];
