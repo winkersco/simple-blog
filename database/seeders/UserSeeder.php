32838aa62ba9f2b6941c79ca3a88f46b7f6f82d2
winkersco <smmehdisharifi@gmail.com>
2023-12-20 01:13:30 +0330

    feat: implement seeder for user

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $allPermissions = Permission::pluck('id', 'id')->all();
+        $adminRole = Role::create(['name' => 'Admin']);
+        $adminRole->syncPermissions($allPermissions);
+        $admin = User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+        ]);
+        $admin->assignRole($adminRole);
+        User::factory()->create([
+            'name' => 'User',
+            'email' => 'user@gmail.com',
+        ]);
+    }
+}
